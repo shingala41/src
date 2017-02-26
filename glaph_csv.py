263680537b0b8af300886509cd22558c13b83fce
shingala41 <shingala41@gmail.com>
2017-02-26 20:57:15 +0900

    comit

---
+++ glaph_csv.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#x,y = np.loadtxt('test01.csv', delimiter=',', unpack=True)
+#array = np.genfromtxt("test01.csv", dtype=None, delimiter=",", unpack=True)
+array = np.genfromtxt("test01.csv", dtype=[("a",int),("b","S2"),("c",int)], delimiter=",")
+
+#print array
+
+array_trans = zip(*array) #tenchi
+
+#print array[0]
+
+#label  = ["MA", "MB", "MC", "MD", "ME"]
+#left   = np.array([1,2])
+#height = np.array([100, 200])
+
+lista = list()
+
+#lista = []
+
+for i in array_trans[2]:
+    array_trans[3].append = 300
+    lista.append(i-20)
+    
+
+plt.bar(array_trans[0], lista, tick_label=array_trans[1], align="center")
+plt.title("Title")
+plt.grid(True)
+
+plt.show()
